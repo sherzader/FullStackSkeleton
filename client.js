b801c9a28b41d849ed5d0a174e08cf67cba3fbd1
Sheri Zada <sheri@hipmunk.com>
2018-01-11 15:59:25 -0800

    use fetch util to render json

--- client.js
+++ client.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import fetch from "./utils/fetch";
+
+const ROUTE = "http://localhost:8888/api/gameState";
+const HEADERS = {};
 
 class App extends Component {
     constructor() {
@@ -7,13 +11,33 @@ class App extends Component {
         this.state = { gameState: {} };
     }
     componentDidMount() {
-        const route = new Request("/api/gameState");
-        fetch(route).then(response =>
-            this.setState({ gameState: response.json() })
-        );
+        this.getGameState();
+    }
+    async getGameState() {
+        const gameState = await fetch(ROUTE, HEADERS);
+        this.setState({ gameState });
     }
     render() {
-        return <div>Ssssss {this.state.gameState}</div>;
+        const { gameState } = this.state;
+        return (
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>User</th>
+                            <th>Result</th>
+                        </tr>
+                        {Object.keys(gameState).map((user, i) => (
+                            <tr key={i}>
+                                <td>{user}</td>
+                                <td>{gameState[user]}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
     }
 }
+
 ReactDOM.render(<App />, document.getElementById("root"));
