a9bafd26f2016d9addb85266e75f7417d4e346a7
Sheri Zada <sheri@hipmunk.com>
2018-01-11 15:56:45 -0800

    handle resource requests, serve bundle.js in html

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const dummyState = { user: "wins" };
 
 const routes = {
     "/api/gameState": handleGameStateRequest,
-    "/": handleRootRequest,
+    "/": handleStaticRequest,
+    "/index.html": handleStaticRequest,
+    "/bundle.js": handleStaticRequest,
 };
-const port = 8000;
+const port = 8888;
 const server = http.createServer().listen(port);
 console.log("server listening on port ", port);
 
@@ -20,33 +22,38 @@ console.log("server listening on port ", port);
  */
 server.on("request", (request, response) => {
     const routeHandler = routes[request.url];
+    const requestURL =
+        request.url === "/" ? indexFile : path.join(__dirname, request.url);
+    console.log("requesting", request.url);
     if (routeHandler) {
-        routeHandler(request, response);
+        routeHandler(requestURL, response);
     } else {
         response.end();
     }
 });
 
-function handleRootRequest(request, response) {
-    console.log("handling root request...");
-
-    fs.readFile("./index.html", (err, html) => {
+function handleStaticRequest(request, response) {
+    fs.readFile(request, (err, html) => {
         console.log("reading file...");
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(html);
+        response.writeHead(200, {
+            "Content-Type": "text/html",
+            "Access-Control-Allow-Origin": "*",
+        });
+        try {
+            response.write(html);
+        } catch (err) {
+            console.log(err);
+        }
         console.log("wrote file...");
         response.end();
     });
 }
 function handleGameStateRequest(request, response) {
-    // fs.readFile("./index.html", (err, data) => {
-    //     const stateString = JSON.stringify(readFromFile());
-    //     response.writeHead(200, { "Content-Type": "text/html" });
-    //     response.write(`${data}<div>${stateString}</div>`);
-    //     // response.write(stateString);
-    //     response.end();
-    // });
     const stateString = JSON.stringify(readFromFile());
+    response.writeHead(200, {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+    });
     response.write(stateString);
     response.end();
 }
